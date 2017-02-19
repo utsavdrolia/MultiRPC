46cdfe52e7902b3c0b30c65d8aef4e53fb7ddd9e
Utsav Drolia <utsavdrolia@gmail.com>
2017-02-19 16:15:23 -0500

    Minor

--- src/org/crowd/rpc/RPCServer.java
+++ src/org/crowd/rpc/RPCServer.java
@@ -32,7 +32,6 @@ public class RPCServer implements RpcController
 
     public RPCServer(String myServiceAddress, Service service, Integer threads)
     {
-        System.out.println("Bound to " + this.myServiceAddress);
         this.myServiceAddress = myServiceAddress;
         executorService = Executors.newFixedThreadPool(threads);
         enqueued = new ConcurrentHashMap<>();
@@ -43,6 +42,7 @@ public class RPCServer implements RpcController
         // Create local server
         serverSock = zContext.createSocket(ZMQ.ROUTER);
         serverSock.bind("tcp://" + this.myServiceAddress);
+        System.out.println("Bound to " + this.myServiceAddress);
         msgProcPipe = ZThread.fork(zContext, new MyMsgProcessor(serverSock));
     }
 
